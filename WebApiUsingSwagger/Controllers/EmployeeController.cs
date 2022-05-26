c161e56b33e2015ce9f49e314969a42db490d0df
sknabirasool <sknabirasool104@gmail.com>
2022-05-26 16:27:27 +0530

    first commit

---
+++ WebApiUsingSwagger/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiUsingSwagger.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: api/Employee
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return GetEmployees();
+        }
+        // GET: api/Employee/5
+        [HttpGet("{id}", Name = "Get")]
+        public Employee Get(int id)
+        {
+            return GetEmployees().Find(e => e.Id == id);
+        }
+        // POST: api/Employee
+        [HttpPost]
+        [Produces("application/json")]
+        public Employee Post([FromBody] Employee employee)
+        {
+            // Logic to create new Employee
+            return new Employee();
+        }
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee employee)
+        {
+            // Logic to update an Employee
+        }
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        private List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName= "John",
+                LastName = "Smith",
+                EmailId ="John.Smith@gmail.com"
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName= "Jane",
+                LastName = "Doe",
+                EmailId ="Jane.Doe@gmail.com"
+            }
+        };
+
+        }
+    }
+}
---
+++ WebApiUsingSwagger/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiUsingSwagger
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailId { get; set; }
+    }
+}
